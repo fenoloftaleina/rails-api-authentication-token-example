1da163152a9d1ff5ac2786ae05fa913a9e557bae
thoulike <thoulike@gmail.com>
2014-11-28 00:08:24 +0100

    Authenticate by tokens.

--- app/controllers/a_controller.rb
+++ app/controllers/a_controller.rb
@@ -1,4 +1,7 @@
 class AController < ApplicationController
+  acts_as_token_authentication_handler_for User, fallback_to_devise: false
+  before_filter :authenticate_user!
+
   def asdf
     render :json => { :message => 'Well done!' }
   end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::API
   include ActionController::MimeResponds
   include ActionController::ImplicitRender
+  extend SimpleTokenAuthentication::ActsAsTokenAuthenticationHandler
 
   respond_to :json
 end
